eb668755d4d2443d5bae93d3f6c581b178ac3f20
Palmer <amp322@kernighan.cs.pitt.edu>
2021-10-05 22:59:53 -0400

    Fixing data collection

--- homework2.sh
+++ homework2.sh
@@ -1,17 +1,5 @@
 #!/bin/sh
-	for i in {1..150}
+	for i in {1..500}
 	do
 		./trap_parallel
-	done
-	clear
-	clear
-	for i in {1..150}
-	do		
-		./mandelbrot_parallel
-	done
-	clear
-	clear
-	for i in {1..150}
-	do
-		./matrix_parallel
 	done
